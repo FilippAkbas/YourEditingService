e12a455946e9148254e346a93ca22f2168da9887
Filipp <filipp.akbas@gmail.com>
2023-07-22 16:32:43 +0300

    Add new pages and update old pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,6 +41,10 @@ const priceSwiper = new Swiper('.price-slider', {
             spaceBetween: 50
         },
         1620: {
+            slidesPerView: 3,
+            spaceBetween: 50
+        },
+        1900: {
             slidesPerView: 4,
             spaceBetween: 50
         }
@@ -48,5 +52,83 @@ const priceSwiper = new Swiper('.price-slider', {
         // If we need pagination
         pagination: {
         el: '.swiper-pagination',
+    },
+});
+const businessSwiper = new Swiper('.business-slider', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    breakpoints: {
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 10
+        },
+        991: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        1366: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        1620: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        }
+    },
+    navigation: {
+        nextEl: '.business-button-next',
+        prevEl: '.business-button-prev',
+    },
+});
+const familySwiper = new Swiper('.family-slider', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    breakpoints: {
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 10
+        },
+        991: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        1366: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        1620: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        }
+    },
+    navigation: {
+        nextEl: '.family-button-next',
+        prevEl: '.family-button-prev',
+    },
+});
+const weddingsSwiper = new Swiper('.weddings-slider', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    breakpoints: {
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 10
+        },
+        991: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        1366: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        1620: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        }
+    },
+    navigation: {
+        nextEl: '.weddings-button-next',
+        prevEl: '.weddings-button-prev',
     },
 });
